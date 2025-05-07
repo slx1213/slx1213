b0fdd4aa760e31018558684075f47d6648fd6075
slx1213 <164445614+slx1213@users.noreply.github.com>
2025-05-07 15:20:47 +0800

    Update CCFTA.py

--- nets/CCFTA.py
+++ nets/CCFTA.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
-# @Author  : Haonan Wang
-# @File    : CTrans.py
-# @Software: PyCharm
-# coding=utf-8
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
